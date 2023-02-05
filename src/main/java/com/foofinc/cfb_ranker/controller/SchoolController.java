3288e8ba6f703f77c04b73047645fd466aec2928
RecursionExcursion <rloup15@gmail.com>
2023-02-05 14:06:24 -0500

    Fixed CORS SSL Error. Unmapped RankingDto and let Spring do it itself

--- src/main/java/com/foofinc/cfb_ranker/controller/SchoolController.java
+++ src/main/java/com/foofinc/cfb_ranker/controller/SchoolController.java
@@ -1,10 +1,12 @@
 package com.foofinc.cfb_ranker.controller;
 
 import com.foofinc.cfb_ranker.service.SchoolService;
+import com.foofinc.cfb_ranker.service.dto.RankingsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,8 +21,9 @@ public class SchoolController {
         this.schoolService = schoolService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/teams")
-    public ResponseEntity<String> getTeams() {
+    public ResponseEntity<RankingsDto> getTeams() {
         return new ResponseEntity<>(schoolService.getTeams(), HttpStatus.OK);
     }
 }
--- src/main/java/com/foofinc/cfb_ranker/service/SchoolService.java
+++ src/main/java/com/foofinc/cfb_ranker/service/SchoolService.java
@@ -1,13 +1,10 @@
 package com.foofinc.cfb_ranker.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foofinc.cfb_ranker.repository.InMemoryRepo;
 import com.foofinc.cfb_ranker.service.dto.RankingDtoMapper;
 import com.foofinc.cfb_ranker.service.dto.RankingsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 public class SchoolService {
@@ -19,13 +16,18 @@ public class SchoolService {
         this.inMemoryRepo = inMemoryRepo;
     }
 
-    public String getTeams() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            RankingsDto rankingsDto = new RankingDtoMapper(inMemoryRepo.getTeams()).mapRankingsToDto();
-            return objectMapper.writeValueAsString(rankingsDto);
-        } catch (IOException e) {
-            throw new RuntimeException("Error occurred while mapping object to JSON");
-        }
+    public RankingsDto getTeams() {
+        return new RankingDtoMapper(inMemoryRepo.getTeams()).mapRankingsToDto();
     }
+
+//    public String getTeams() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            RankingsDto rankingsDto = new RankingDtoMapper(inMemoryRepo.getTeams()).mapRankingsToDto();
+////            return objectMapper.writeValueAsString(rankingsDto);
+//            return objectMapper.writeValueAsString(rankingsDto);
+//        } catch (IOException e) {
+//            throw new RuntimeException("Error occurred while mapping object to JSON");
+//        }
+//    }
 }
